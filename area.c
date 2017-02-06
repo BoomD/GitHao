c5ce5333d01ba328ca7593ad668ba92ff735e744
Hao Dong <hdmpf@mail.missouri.edu>
2017-02-06 17:40:55 -0600

    Part 1

--- area.c
+++ area.c
@@ -5,13 +5,14 @@
 #define pi 3.1415926
 
 int main(int argc, const char * argv[]) {
-    double radius, area;
+    double radius, cir;
     printf("Make Changes");
+    printf("
     printf("This is a test comment\n");
     printf("Enter the radius:\n");
     scanf("%lf",&radius);
-    area=pi*pow(radius, 2);//use power function to calculate the area of given radius
-    printf("Area of radius=%lf is %.2lf\n",radius,area);
+    cir=pi*radius*2;//use power function to calculate the area of given radius
+    printf("Circumference of radius=%lf is %.2lf\n",cir);
     
     
 }
